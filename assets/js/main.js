8b7d28be8316c3cf37ab12e9468cf6c360ab2f87
Sang Vo <stvo1589@gmail.com>
2024-10-29 11:31:27 -0700

    fixed the bugs where theme does not stay after switching pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,63 +5,63 @@
  */
 
 (() => {
-    'use strict'
+    "use strict"
 
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-    const getStoredTheme = () => localStorage.getItem('theme');
-    const setStoredTheme = theme => localStorage.setItem('theme', theme);
+    const tooltipTriggerList = document.querySelectorAll("[data-bs-toggle='tooltip']");
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+    const getStoredTheme = () => localStorage.getItem("theme");
+    const setStoredTheme = theme => localStorage.setItem("theme", theme);
 
     const getPreferredTheme = () => {
         const storedTheme = getStoredTheme();
         if (storedTheme) {
-            return storedTheme
+            return storedTheme;
         }
 
-        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+        return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
     }
 
     const setTheme = theme => {
-        if (theme === 'auto') {
-            document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'));
+        if (theme === "auto") {
+            document.documentElement.setAttribute("data-bs-theme", (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"));
         } else {
-            document.documentElement.setAttribute('data-bs-theme', theme);
+            document.documentElement.setAttribute("data-bs-theme", theme);
         }
     }
 
     setTheme(getPreferredTheme())
 
     const showActiveTheme = (theme, focus = false) => {
-        const themeSwitcher = document.querySelector('#bd-theme');
+        const themeSwitcher = document.querySelector("#bd-theme");
 
         if (!themeSwitcher) {
-            return
+            return;
         }
 
-        const themeSwitcherText = document.querySelector('#bd-theme-text');
-        const activeThemeIcon = document.querySelector('.theme-icon-active use');
+        const themeSwitcherText = document.querySelector("#bd-theme-text");
+        const activeThemeIcon = document.querySelector(".theme-icon-active use");
         const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`);
-        const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href');
+        const svgOfActiveBtn = btnToActive.querySelector("svg use").getAttribute("href");
 
-        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-            element.classList.remove('active');
-            element.setAttribute('aria-pressed', 'false');
+        document.querySelectorAll("[data-bs-theme-value]").forEach(element => {
+            element.classList.remove("active");
+            element.setAttribute("aria-pressed", "false");
         });
 
-        btnToActive.classList.add('active');
-        btnToActive.setAttribute('aria-pressed', 'true');
-        activeThemeIcon.setAttribute('href', svgOfActiveBtn);
+        btnToActive.classList.add("active");
+        btnToActive.setAttribute("aria-pressed", "true");
+        activeThemeIcon.setAttribute("href", svgOfActiveBtn);
         const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
-        themeSwitcher.setAttribute('aria-label', themeSwitcherLabel);
+        themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
 
         if (focus) {
             themeSwitcher.focus();
         }
     };
 
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => {
         const storedTheme = getStoredTheme();
-        if (storedTheme !== 'light' && storedTheme !== 'dark') {
+        if (storedTheme !== "light" && storedTheme !== "dark") {
             setTheme(getPreferredTheme());
         }
     });
@@ -78,10 +78,10 @@
     window.addEventListener("DOMContentLoaded", () => {
         showActiveTheme(getPreferredTheme());
 
-        document.querySelectorAll('[data-bs-theme-value]')
+        document.querySelectorAll("[data-bs-theme-value]")
             .forEach(toggle => {
-                toggle.addEventListener('click', () => {
-                    const theme = toggle.getAttribute('data-bs-theme-value');
+                toggle.addEventListener("click", () => {
+                    const theme = toggle.getAttribute("data-bs-theme-value");
                     setStoredTheme(theme);
                     setTheme(theme);
                     showActiveTheme(theme, true);
@@ -137,7 +137,7 @@
 
                     navigator.clipboard.writeText(textToCopy).then(() => {
                         copyButton.classList.replace("btn-primary", "btn-outline-primary");
-                        copyButton.setAttribute('data-bs-original-title', 'Copied!');
+                        copyButton.setAttribute("data-bs-original-title", "Copied!");
 
                         const icon = copyButton.querySelector("i");
                         if (icon) {
